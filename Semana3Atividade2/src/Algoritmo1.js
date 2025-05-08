b4739ab5c429fa50e50888d0973432c3f37b292d
Maria Eduarda <eduardabritto2014@gmail.com>
2025-05-08 15:46:43 -0300

    Add files via upload

---
+++ Semana3Atividade2/src/Algoritmo1.js
@@ -0,0 +1,18 @@
+const tamanho = prompt('Digite o tamanho da lista de números');
+// Solicita ao usuário um número 
+// Define o tamanho de elementos que a lista deve ter
+
+const lista = new Array(tamanho).fill(null);
+// cria um array com tamanho preenchido com null usando o fill
+// inicia uma lista vazia com o número de elementos definidos 
+
+const resultado = lista
+    .map((item, index) => index + 1) // usa o index para criar uma sequência de números
+    // cria uma sequência numérica iniciando em 1 até o valor de `tamanho`
+
+    .reduce((acumulador, item) => acumulador * item);
+//  Utiliza reduce para multiplicar todos os elementos da lista
+
+console.log(resultado);
+//  Exibe o resultado final no console
+
